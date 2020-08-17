c76422a3b1d885c4d6ddce10ab2473bcaed38e01
SeungminKim <ksm0057@naver.com>
2020-08-17 18:14:10 +0900

    Add geo

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import React from "react";
 import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return <Loading />;
+export default class extends React.Component {
+  getLoaction = async () => {
+    const location = await Location.getCurrentPositionAsync();
+    console.log(location);
+  };
+  componentDidMount() {
+    this.getLoaction();
+  }
+  render() {
+    return <Loading />;
+  }
 }
