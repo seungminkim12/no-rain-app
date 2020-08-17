5c05515f420e89c68b72a7ab892f2048e95481be
SeungminKim <ksm0057@naver.com>
2020-08-17 18:54:52 +0900

    idk

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
 import React from "react";
 import Loading from "./Loading";
+import { Alert } from "react-native";
 import * as Location from "expo-location";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
   getLoaction = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      console.log(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Cant find you.", "So sad");
+    }
   };
   componentDidMount() {
     this.getLoaction();
   }
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
